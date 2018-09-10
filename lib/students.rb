f4f1917426934751e3d3aa5989232a31d022663f
Cedric David <cedric.david@btopenworld.com>
2018-09-10 01:16:17 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,7 +13,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  "SELECT COUNT(gpa) FROM students"
+  "SELECT COUNT(tardies) FROM students"
 end
 
 def average_gpa_for_9th_grade
