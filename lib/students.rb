1f39b5da7a92ce0844405deb2ba944da49672ebd
Cedric David <cedric.david@btopenworld.com>
2018-09-10 01:18:39 +0000

    Done.

--- lib/students.rb
+++ lib/students.rb
@@ -5,17 +5,17 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  "SELECT MIN(gpa) FROM students;"
+  "SELECT MIN(gpa) FROM students"
 end
 
 def average_student_gpa
-  "SELECT AVG(gpa) FROM students;"
+  "SELECT AVG(gpa) FROM students"
 end
 
 def total_tardies_for_all_students
-  "SELECT SUM(tardies) FROM students;"
+  "SELECT SUM(tardies) FROM students"
 end
 
 def average_gpa_for_9th_grade
-  "SELECT AVG(gpa) FROM students WHERE grade == 9 ;"
+  "SELECT AVG(gpa) FROM students WHERE grade = 9"
 end
